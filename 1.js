303cb1759f8605026e99cd25f213fa6398c538b8
vinay <107908969+vinay-youngman@users.noreply.github.com>
2023-09-22 07:45:07 +0000

    oo

---
+++ 1.js
@@ -0,0 +1,11 @@
+// console.log('dfdsgf')
+
+// let isLoggedIN = 1
+
+// let v = Boolean(isLoggedIN)
+// console.log(v)
+
+let someNUmber = 33
+let stringNumber = String(someNUmber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
